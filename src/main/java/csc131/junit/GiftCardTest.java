b1ecd52a88639a1913402d9dc222d1af32984334
Visoth <blacksterteoz@gmail.com>
2020-11-15 20:07:15 -0800

    the lab assignment testing

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -6,16 +6,23 @@ import org.junit.rules.ExpectedException;
 
 public class GiftCardTest
 {
- @Test
- public void getIssuingStore()
- {
- double balance;
- GiftCard card;
- int issuingStore;
+    @Test
+    public void deduct_RemainingBalance()
+    {
+        double       balance;        
+        GiftCard     card;
+        int          issuingStore;
+        String       s;
+        
+        
+        issuingStore = 1337;
+        balance      = 100.00;
+        card = new GiftCard(issuingStore, balance);
 
- issuingStore = 1337;
- balance = 100.00;
- card = new GiftCard(issuingStore, balance);
- assertEquals("getBalance()",balance, card.getBalance(),0.001);
+        s = "Remaining Balance: " + String.format("%6.2f", 90.00);
+        assertEquals("deduct(10.00)", 
+                     s, card.deduct(10.0));
+    }
+
  }
 }
